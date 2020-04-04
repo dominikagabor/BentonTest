ab22f59d6b4c9808ea7ce2de624fbfbf426a4f38
Dominika Gabor <42026314+dominikagabor@users.noreply.github.com>
2020-04-04 21:22:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,17 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'dbbenton'
 mysql.init_app(app)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+
+@app.route('/', methods=['GET'])
+def login():      
+        return render_template("app.html")
+
+@app.route('/signup', methods=['GET'])
+def signup():      
+        return render_template("signup.html")
+
+@app.route('/', methods=['POST'])
+def login_post():
     try:
         if request.method == 'POST':
             email = request.form['email']
@@ -21,37 +30,55 @@ def index():
             password = request.form.get('password')   
             print("Password app: " + password)
 
-            cur = mysql.connect().cursor()
-            cur.execute("SELECT count(email) FROM login where email = " + "'" + email + "'")
-            countEmailsDB = cur.fetchall()
-
-            for row in countEmailsDB:
-                countEmailDB = row[0]
-
-            if countEmailDB != 0:
-                cur = mysql.connect().cursor()
-                cur.execute("SELECT email FROM login where email = " + "'" + email + "'")
-                emailsDB = cur.fetchall()
-                for row in emailsDB:
-                    emailDB = row[0]
-
-                cur = mysql.connect().cursor()
-                cur.execute("SELECT password FROM login where password = " + "'" + password + "'")
-                passwordDB = cur.fetchall()
-                for row in passwordDB:
-                    passwordDB = row[0]
-                    print("Password db: " + str(passwordDB))
-
-                if str(emailDB) == email and str(passwordDB) == password:
-                     return render_template("main.html")
-                if str(emailDB) != email or str(passwordDB) != password:
-                    return render_template("app.html", loginMessage = 'Nieprawidłowy login lub hasło')
+            if email == "" or password == "":
+                return render_template("app.html", loginMessage = 'Login lub hasło nie mogą być puste')
             else:
-                return render_template("app.html", loginMessage = 'Nieprawidłowy login lub hasło')
+                cur = mysql.connect().cursor()
+                cur.execute("SELECT count(email) FROM login where email = " + "'" + email + "'")
+                countEmailsDB = cur.fetchall()
+
+                for row in countEmailsDB:
+                    countEmailDB = row[0]
+
+                if countEmailDB != 0:
+                    cur = mysql.connect().cursor()
+                    cur.execute("SELECT email FROM login where email = " + "'" + email + "'")
+                    emailsDB = cur.fetchall()
+                    for row in emailsDB:
+                        emailDB = row[0]
+
+                    cur = mysql.connect().cursor()
+                    cur.execute("SELECT password FROM login where password = " + "'" + password + "'")
+                    passwordDB = cur.fetchall()
+                    for row in passwordDB:
+                        passwordDB = row[0]
+                        print("Password db: " + str(passwordDB))
+            
+
+                    cur = mysql.connect().cursor()
+                    cur.execute("SELECT name FROM login where email = " + "'" + email + "'" + "and password = " + "'" + password + "'")
+                    namesDB = cur.fetchall()
+
+                    for row in namesDB:
+                        nameDB = row[0]
+                        print("Name db: " + str(nameDB))
+
+                
+                    if str(emailDB) == email and str(passwordDB) == password:
+                        return render_template("main.html", name = nameDB)
+                    if str(emailDB) != email or str(passwordDB) != password:
+                        return render_template("app.html", loginMessage = 'Nieprawidłowy login lub hasło')
+                else:
+                    return render_template("app.html", loginMessage = 'Nieprawidłowy login lub hasło')
           
         return render_template("app.html", loginMessage = '')
     except Exception as e:
         return str(e)
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    return render_template("signup.html")
+
 
 if __name__ == '__main__':
     app.run()
