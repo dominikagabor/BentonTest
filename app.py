cb052392673c264b343567ef20384b73916df428
Dominika Gabor <42026314+dominikagabor@users.noreply.github.com>
2020-04-05 20:36:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'dbbenton'
 mysql.init_app(app)
 
-
 @app.route('/', methods=['GET'])
 def login():      
         return render_template("app.html")
@@ -17,6 +16,10 @@ def login():
 @app.route('/signup', methods=['GET'])
 def signup():      
         return render_template("signup.html")
+
+@app.route('/profile', methods=['GET'])
+def profile():      
+        return render_template("profile.html")
 
 @app.route('/', methods=['POST'])
 def login_post():
@@ -70,14 +73,104 @@ def login_post():
                         return render_template("app.html", loginMessage = 'Nieprawidłowy login lub hasło')
                 else:
                     return render_template("app.html", loginMessage = 'Nieprawidłowy login lub hasło')
-          
+        actualEmail = email
+        print("Actual email: " + actualEmail)
         return render_template("app.html", loginMessage = '')
     except Exception as e:
         return str(e)
 
 @app.route('/signup', methods=['POST'])
 def signup_post():
-    return render_template("signup.html")
+    try:
+        if request.method == 'POST': 
+            name = request.form['name']
+            name = request.form.get('name')
+            print("Name app: " + name)
+
+            email = request.form['email']
+            email = request.form.get('email')
+            print("Email app: " + email)
+
+            password = request.form['password']     
+            password = request.form.get('password')   
+            print("Password app: " + password)
+
+            repeatPassword = request.form['repeatPassword']     
+            repeatPassword = request.form.get('repeatPassword')   
+            print("RepeatPassword app: " + repeatPassword)
+
+            countEmails = 0
+            cur = mysql.connect().cursor()
+            cur.execute("SELECT email FROM login")
+            emailsDB = cur.fetchall()
+            for row in emailsDB:
+                emailDB = row[0]
+                print(emailDB)
+                if emailDB == email:
+                     countEmails = countEmails + 1
+                     return render_template("signup.html", signupMessage = 'Taki email istnieje w bazie.')  
+
+            if countEmails == 0:
+                if name == "" or email == "" or password == "" or repeatPassword == "":
+                    return render_template("signup.html", signupMessage = 'Wszystkie pola muszą być uzupełnione.')
+                else:
+                    if password == repeatPassword:
+                        try:
+                            cur = mysql.connect().cursor()
+                            cur.execute("INSERT INTO login(email, password, name) values('" + email + "', '" + password + "', '" + name + "')")
+                            print("INSERT INTO login(email, password, name) values('" + email + "', '" + password + "', '" + name + "')")
+                            mysql.connect().commit()
+                            cur.close()
+                            print("zapisane w bazie")
+                        except Exception as e:
+                            return str(e)
+                    
+                    else:
+                        return render_template("signup.html", signupMessage = 'Hasła różnią się od siebie.')  
+        return render_template("app.html", correctSignupMessage = "Konto założone poprawnie. Zaloguj się!")
+    except Exception as e:
+        return str(e)
+
+@app.route('/profile', methods=['POST'])
+def profile_post(actualEmail):
+    try:
+        if request.method == 'POST': 
+            print("Actual email: " + actualEmail)
+            name = request.form['name']
+            name = request.form.get('name')
+            print("Name profile app: " + name)
+
+            password = request.form['password']     
+            password = request.form.get('password')   
+            print("Password profile app: " + password)
+
+            repeatPassword = request.form['repeatPassword']     
+            repeatPassword = request.form.get('repeatPassword')   
+            print("RepeatPassword profile app: " + repeatPassword)
+
+            countEmails = 0
+            cur = mysql.connect().cursor()
+            cur.execute("SELECT email FROM login where email = " + "'" + actualEmail + "'")
+            emailsDB = cur.fetchall()
+            for row in emailsDB:
+                emailDB = row[0]
+                print(emailDB)
+                if emailDB == actualEmail:
+                     countEmails = countEmails + 1
+
+
+            if countEmails == 1:
+                    if password == repeatPassword:
+                        cur = mysql.connect().cursor()
+                        cur.execute("INSERT INTO login(password, name) values('" + password + "', '" + name + "') where email = " + "'" + actualEmail + "'")
+                        print("INSERT INTO login(password, name) values('" + password + "', '" + name + "') where email = " + "'" + actualEmail + "'")
+                        mysql.connect().commit()
+                        print("zapisane w bazie")
+                    else:
+                        return render_template("profile.html", editMessage = 'Hasła różnią się od siebie.')  
+        return render_template("main.html", correctEditMessage = "Dane zapisane poprawnie.")
+    except Exception as e:
+        return str(e)
 
 
 if __name__ == '__main__':
